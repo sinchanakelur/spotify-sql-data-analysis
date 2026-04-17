9e9e9685b77c54b45a4289b1822c77e4d4693659
sinchanakelur <sinchanakelur452005@gmail.com>
2026-04-17 11:20:33 +0530

    Update easy.sql

--- queries/easy.sql
+++ queries/easy.sql
@@ -2,31 +2,37 @@
 -- EASY LEVEL QUERIES
 -- =========================================
 
--- 1. Retrieve the names of all tracks that have more than 1 billion streams
+
+-- 1. Tracks with more than 1 billion streams
+-- Filters tracks based on high streaming count
 SELECT track
 FROM spotify
 WHERE stream > 1000000000;
 
 
--- 2. List all albums along with their respective artists
+-- 2. Albums with their respective artists
+-- Retrieves unique album-artist combinations
 SELECT DISTINCT album, artist
 FROM spotify
 ORDER BY album;
 
 
--- 3. Get the total number of comments for tracks where licensed = TRUE
+-- 3. Total comments for licensed tracks
+-- Aggregates comments where licensed is TRUE
 SELECT SUM(comments) AS total_comments
 FROM spotify
 WHERE licensed = TRUE;
 
 
--- 4. Find all tracks that belong to the album type 'single'
+-- 4. Tracks belonging to 'single' album type
+-- Filters tracks with album type as 'single' (case-insensitive)
 SELECT track
 FROM spotify
 WHERE album_type ILIKE 'single';
 
 
--- 5. Count the total number of tracks by each artist
+-- 5. Total number of tracks by each artist
+-- Counts tracks grouped by artist and sorts by highest count
 SELECT artist, COUNT(track) AS total_tracks
 FROM spotify
 GROUP BY artist
